374db8ba4c641afa547b8f104b8ee08a0c1be0d4
unknown <emmanuel.james@gmail.com>
2023-02-06 17:59:17 -0500

    db2

--- crud/settings.py
+++ crud/settings.py
@@ -24,7 +24,8 @@ TAILWIND_APP_NAME = 'theme'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+#DEBUG = 'RENDER' not in os.environ
+DEBUG= True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
